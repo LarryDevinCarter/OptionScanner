6c9c1357e21bfebe035c5870b5ea10f56c13ebd4
LarryDevinCarter <larrydevincarter@gmail.com>
2025-03-17 14:41:58 -0500

    first commit

---
+++ option-scanner/src/main/java/com/larrydevincarter/optionscanner/OptionScannerApplication.java
@@ -0,0 +1,13 @@
+package com.larrydevincarter.optionscanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptionScannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptionScannerApplication.class, args);
+	}
+
+}
