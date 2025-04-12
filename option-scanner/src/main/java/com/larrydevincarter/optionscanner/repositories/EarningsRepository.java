858fcc198079e5d17905fe3b6d63c47055ddce34
LarryDevinCarter <larrydevincarter@gmail.com>
2025-04-12 06:25:17 -0500

    updated EarningsRepo to more accuritly select only symbols that need earnings updated

--- option-scanner/src/main/java/com/larrydevincarter/optionscanner/repositories/EarningsRepository.java
+++ option-scanner/src/main/java/com/larrydevincarter/optionscanner/repositories/EarningsRepository.java
@@ -4,6 +4,7 @@ import com.larrydevincarter.optionscanner.entities.Earnings;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -13,4 +14,20 @@ public interface EarningsRepository extends JpaRepository<Earnings, Long> {
 
     @Query("SELECT DISTINCT e.symbol FROM Earnings e WHERE e.lastUpdated >= :startOfDay")
     List<String> findSymbolsUpdatedToday(LocalDateTime startOfDay);
+
+    @Query("SELECT DISTINCT a.symbol " +
+            "FROM Asset a " +
+            "INNER JOIN IncomeStatement i ON a.symbol = i.symbol " +
+            "LEFT JOIN Earnings e ON a.symbol = e.symbol " +
+            "WHERE a.status = 'active' AND a.tradable = true " +
+            "AND i.fiscalDateEnding >= :date " +
+            "AND (e.symbol IS NULL OR " +
+            "     a.symbol NOT IN (" +
+            "         SELECT e2.symbol " +
+            "         FROM Earnings e2 " +
+            "         WHERE e2.fiscalDateEnding >= :date " +
+            "         AND e2.symbol = a.symbol " +
+            "     )" +
+            ")")
+    List<String> findActiveTradableSymbolsNeedingUpdate(LocalDate date);
 }
