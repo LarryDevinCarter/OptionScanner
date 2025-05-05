766be4f9f881eec194f0efd66769141d75babc8b
LarryDevinCarter <larrydevincarter@gmail.com>
2025-05-05 11:35:20 -0500

    error report for development purposes

--- option-scanner/src/main/java/com/larrydevincarter/optionscanner/repositories/EarningsRepository.java
+++ option-scanner/src/main/java/com/larrydevincarter/optionscanner/repositories/EarningsRepository.java
@@ -3,11 +3,13 @@ package com.larrydevincarter.optionscanner.repositories;
 import com.larrydevincarter.optionscanner.entities.Earnings;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface EarningsRepository extends JpaRepository<Earnings, Long> {
 
     void deleteBySymbol(String symbol);
--- option-scanner/src/main/java/com/larrydevincarter/optionscanner/repositories/IncomeStatementRepository.java
+++ option-scanner/src/main/java/com/larrydevincarter/optionscanner/repositories/IncomeStatementRepository.java
@@ -4,12 +4,14 @@ import com.larrydevincarter.optionscanner.entities.IncomeStatement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface IncomeStatementRepository extends JpaRepository<IncomeStatement, Long> {
 
     @Query("SELECT DISTINCT a.symbol " +
